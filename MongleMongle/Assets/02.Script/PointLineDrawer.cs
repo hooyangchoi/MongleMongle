c7ce9475c1c395baaf0b1aef4b0f1d40132d605a
hooyangchoi <31362640+hooyangchoi@users.noreply.github.com>
2018-01-27 23:14:33 +0900

    qwe
    qwe

--- MongleMongle/Assets/02.Script/PointLineDrawer.cs
+++ MongleMongle/Assets/02.Script/PointLineDrawer.cs
@@ -66,7 +66,7 @@ public class PointLineDrawer : MonoBehaviour
 
                     if(LineHitChecker())
                     {
-                        Debug.Log("Fuck");
+                        //Debug.Log("Fuck");
                     }
                 }
             }
